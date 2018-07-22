7028c51eb6d6f2b427cb03d56e7b26e6e598e9fa
Rd Islam Nasim <rdislamnasim78@gmail.com>
2018-07-22 17:45:11 +0600

    Update project

--- app/src/main/java/com/terminalbd/isurance/RegisLogActivity.java
+++ app/src/main/java/com/terminalbd/isurance/RegisLogActivity.java
@@ -1,13 +1,28 @@
 package com.terminalbd.isurance;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisLogActivity extends AppCompatActivity {
+
+    Button cancleBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regis_log);
     }
+
+    public void onClickSignUp(View view) {
+
+    }
+
+    /*public void onClickCancle(View view) {
+        Intent intent = new Intent(this, YesFragment.class);
+        cancleBtn = findViewById(R.id.regCancelBtn);
+        startActivity(intent);
+    }*/
 }
--- app/src/main/java/com/terminalbd/isurance/YesFragment.java
+++ app/src/main/java/com/terminalbd/isurance/YesFragment.java
@@ -1,21 +1,40 @@
 package com.terminalbd.isurance;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 ;
 
 
 public class YesFragment extends Fragment {
+    Activity context;
 
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_yes, container, false);
+       context=getActivity();
+
+       return inflater.inflate(R.layout.fragment_yes, container, false);
 
     }
 
+    public void onStart(){
+        super.onStart();
+        Button freeQuota = context.findViewById(R.id.freeQuota);
 
+        freeQuota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent intent = new Intent(context, RegisLogActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
